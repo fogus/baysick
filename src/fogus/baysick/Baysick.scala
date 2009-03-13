1a2d2e533e62a897acfbe2d07536729c3d9f0c25
Fogus <mefogus@gmail.com>
2009-03-13 13:39:14 -0400

    The most simple BASIC commands.  print, goto, and run

---
+++ src/fogus/baysick/Baysick.scala
@@ -0,0 +1,26 @@
+package fogus.baysick {
+  class Baysick {
+    abstract sealed class BasicLine
+    case class PrintLine(num: Int, s: String) extends BasicLine
+    case class GotoLine(num: Int, to: Int) extends BasicLine
+    val lines = new scala.collection.mutable.HashMap[Int, BasicLine]
+
+    case class linebuilder(num: Int) {
+      def GOTO(to: Int) = lines(num) = GotoLine(num, to)
+      def PRINT(s: String) = lines(num) = PrintLine(num, s)
+    }
+
+    private def gotoLine(line: Int) {
+      lines(line) match {
+        case PrintLine(_, s) => println(s); gotoLine(line + 10)
+        case GotoLine(_, to) => gotoLine(to)
+      }
+    }
+
+    def RUN {
+      gotoLine(lines.keys.toList.first)
+    }
+
+    implicit def int2linebuilder(i: Int) = linebuilder(i)
+  }
+}
