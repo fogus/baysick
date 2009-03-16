1d269455501826052e412dcf203869f75d7e9613
Fogus <mefogus@gmail.com>
2009-03-16 11:19:45 -0400

    Adding end support

--- src/fogus/baysick/scripts/BasickTests.scala
+++ src/fogus/baysick/scripts/BasickTests.scala
@@ -15,6 +15,24 @@ object EndlessLoop extends Baysick {
   def main(args:Array[String]) = {
     10 PRINT "Fogus Rulez!"
     20 GOTO 10
+    30 END
+
+    RUN
+  }
+}
+
+object HelloWorld extends Baysick {
+  def main(args:Array[String]) = {
+    10 PRINT "Hello Cleveland!"
+    20 END
+
+    RUN
+  }
+}
+
+object HelloName extends Baysick {
+  def main(args:Array[String]) = {
+    10 INPUT ("What is your name: ", 'n)
 
     RUN
   }
