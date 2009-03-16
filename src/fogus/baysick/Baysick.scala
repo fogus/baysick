d46b9cb2449b403ab2fbd63c3c4b69c610f976ea
Fogus <mefogus@gmail.com>
2009-03-16 11:19:41 -0400

    Adding end support

--- src/fogus/baysick/Baysick.scala
+++ src/fogus/baysick/Baysick.scala
@@ -5,17 +5,35 @@ package fogus.baysick {
     abstract sealed class BasicLine
     case class PrintLine(num: Int, s: String) extends BasicLine
     case class GotoLine(num: Int, to: Int) extends BasicLine
+    case class InputLine(num: Int, msg: String, name: Symbol) extends BasicLine
+    case class EndLine(num: Int) extends BasicLine
+
     val lines = new HashMap[Int, BasicLine]
+    val binds = new HashMap[Symbol, Any]
 
     case class linebuilder(num: Int) {
       def GOTO(to: Int) = lines(num) = GotoLine(num, to)
       def PRINT(s: String) = lines(num) = PrintLine(num, s)
+      def INPUT(msg: String, name: Symbol) = lines(num) = InputLine(num, msg, name)
+      def END() = lines(num) = EndLine(num)
     }
 
     private def gotoLine(line: Int) {
       lines(line) match {
-        case PrintLine(_, s) => println(s); gotoLine(line + 10)
+        case PrintLine(_, s) => {
+          println(s)
+          gotoLine(line + 10)
+        }
+        case InputLine(_, msg, name) => {
+          println("I will display" + msg)
+          println("Then I will lookup " + name)
+          gotoLine(line + 10)
+        }
         case GotoLine(_, to) => gotoLine(to)
+        case EndLine(_) => {
+          println("Done.")
+          System.exit(0)
+        }
       }
     }
 
