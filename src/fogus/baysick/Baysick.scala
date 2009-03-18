42d675b56e86bc1e8d652c415baa6ecb5b5c6516
Fogus <mefogus@gmail.com>
2009-03-18 13:07:20 -0400

    LET form working, but requires parens?

--- src/fogus/baysick/Baysick.scala
+++ src/fogus/baysick/Baysick.scala
@@ -16,7 +16,7 @@ package fogus.baysick {
     val binds = new HashMap[Symbol, Any]
 
     case class Assignr(sym:Symbol) {
-      def :=(value:String):Function0[Unit] = {
+      def :=(value:Any):Function0[Unit] = {
         return new Function0[Unit] {
           def apply() = {
             binds(sym) = value
