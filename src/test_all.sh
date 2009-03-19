f90eaafa13adeea2dec82136371cf14ba956da2f
Fogus <mefogus@gmail.com>
2009-03-19 15:45:42 -0400

    run all tests

---
+++ src/test_all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloWorld
+java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloNumbers
+java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloName
+java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloPrint
+java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloLet
+java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloIf
+java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar SquareRoot
