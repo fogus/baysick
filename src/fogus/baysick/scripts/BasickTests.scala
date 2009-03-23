0d9fecb56af535e905fbefac9555b238f44461af
Fogus <mefogus@gmail.com>
2009-03-23 08:53:08 -0400

    This is what it should look like at the end

--- src/fogus/baysick/scripts/BasickTests.scala
+++ src/fogus/baysick/scripts/BasickTests.scala
@@ -136,21 +136,18 @@ object Lunar extends Baysick {
     120 IF ABS('burn) <= 'fuel THEN 150
     130 PRINT "You don't have that much fuel"
     140 GOTO 100
-    150 LET 'v = 'v + 'burn * 10 / ('fuel + 'mass)
-/*
-130 LET fuel = fuel - ABS(burn)
-140 LET dist = dist - v
-150 IF dist > 0 THEN 100
-160 PRINT "You have hit the surface"
-170 IF v < 3 THEN 210
-180 PRINT "Hit surface too fast (", v,")km/s"
-190 PRINT "You Crash"
-200 GOTO 220
-210 PRINT "Well done"
-220 REM END
+    150 LET ('v := 'v + 'burn * 10 / ('fuel + 'mass))
+    160 LET ('fuel := 'fuel - ABS('burn))
+    170 LET ('dist := 'dist - 'v)
+    180 IF 'dist > 0 THEN 100
+    190 PRINT "You have hit the surface"
+    200 IF 'v < 3 THEN 240
+    210 PRINT "Hit surface too fast (" % 'v % ")km/s"
+    220 PRINT "You Crashed!"
+    230 GOTO 250
+    240 PRINT "Well done"
 
-*/
-    120 END
+    250 END
 
     RUN
   }
