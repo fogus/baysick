27cd851df4640335ff95546c965cbd7b083b1365
fogus <mefogus@gmail.com>
2014-05-11 08:47:39 -0400

    License file

--- src/fogus/baysick/scripts/BasickTests.scala
+++ src/fogus/baysick/scripts/BasickTests.scala
@@ -4,9 +4,12 @@
  * Licensed under The MIT License
  * Re-distributions of files must retain the above copyright notice.
  *
- * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ *  #######        ##      ##    ##    ######     ####     ######     ##   ##
+ *  ##    ##      ####      ##  ##    ##           ##     ##    ##    ##  ##
+ *  #######      ##  ##       ##       ######      ##     ##          ####
+ *  ##    ##    ########      ##            ##     ##     ##    ##    ##  ##
+ *  #######    ##      ##     ##       ######     ####     ######     ##   ##
  **/
-
 import fogus.baysick.Baysick
 
 /**
