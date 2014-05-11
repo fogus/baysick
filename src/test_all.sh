8381aa4ad7537bc22fe40aee954063d1990159b4
fogus <mefogus@gmail.com>
2014-05-11 08:48:43 -0400

    License file

--- src/test_all.sh
+++ src/test_all.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2010-2014 Michael Fogus, http://www.fogus.me
+#
+# Licensed under The MIT License
+# Re-distributions of files must retain the above copyright notice.
+#
+#  #######        ##      ##    ##    ######     ####     ######     ##   ##
+#  ##    ##      ####      ##  ##    ##           ##     ##    ##    ##  ##
+#  #######      ##  ##       ##       ######      ##     ##          ####
+#  ##    ##    ########      ##            ##     ##     ##    ##    ##  ##
+#  #######    ##      ##     ##       ######     ####     ######     ##   ##
+
 java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloWorld
 java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloNumbers
 java -cp $SCALA_HOME/lib/scala-library.jar:baysick.jar HelloName
